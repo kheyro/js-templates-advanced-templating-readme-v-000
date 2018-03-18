22b06d6da99c3b841bbbfafcadc2014037cfeeb0
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-18 20:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function loadIssue() {
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
 
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
+
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issues);
